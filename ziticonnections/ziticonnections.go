1833336fdffa2375c2afd8c7c89df2313d67930d
Nick Pieros <nick.pieros@netfoundry.io>
2021-10-26 18:31:30 -0400

    started working on changes to allow ziti and no ziti connections as well as move the location of the ziti config file path inside the prometheus config file

--- ziticonnections/ziticonnections.go
+++ ziticonnections/ziticonnections.go
@@ -5,7 +5,9 @@ import (
 	"net"
 	"os"
 	"strings"
+	"time"
 
+	"github.com/mwitkow/go-conntrack"
 	"github.com/openziti/sdk-golang/ziti"
 	"github.com/openziti/sdk-golang/ziti/config"
 	"github.com/sirupsen/logrus"
@@ -15,18 +17,34 @@ var zitiConfigFile string
 
 func GetZitiDialContextFunction(ctx context.Context, network string, addr string) (net.Conn, error) {
 
-	//service := "nick service"
 	configFile, err := config.NewFromFile(zitiConfigFile)
 
 	if err != nil {
 		logrus.WithError(err).Error("Error loading ziti config file")
 		os.Exit(1)
 	}
+	addressString := strings.Split(addr, ":")[0]
+	zitiDialInfo := strings.Split(addressString, ".")
 
-	serviceName := strings.Split(addr, ":")[0]
+	if strings.ToLower(zitiDialInfo[1]) == "ziti" {
+		zitiDial := strings.Split(zitiDialInfo[0], "-")
+		serviceName := zitiDial[0]
+		identity := zitiDial[1]
 
-	context := ziti.NewContextWithConfig(configFile)
-	return context.Dial(serviceName)
+		dialOptions := &ziti.DialOptions{
+			Identity:       identity,
+			ConnectTimeout: time.Minute,
+		}
+
+		context := ziti.NewContextWithConfig(configFile)
+		return context.DialWithOptions(serviceName, dialOptions)
+	} else {
+		dialContext := conntrack.NewDialContextFunc(
+			conntrack.DialWithTracing(),
+			conntrack.DialWithName(addr))
+
+		return dialContext(ctx, network, addr)
+	}
 }
 
 func SetZitiConfigFile(filePath string) {
